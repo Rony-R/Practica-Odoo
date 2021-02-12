76649faf6355efea4cd6154450551ec67d2ca9a9
Rony-R <ronyrodriguez2011@hotmail.com>
2021-02-11 23:06:46 -0600

    Sessions Tree

--- openacademy/models/course.py
+++ openacademy/models/course.py
@@ -9,5 +9,4 @@ class Course(models.Model):
     name = fields.Char(string="Title", required=True)
     description = fields.Text()
 
-    
-    #responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string="Responsible", index=True)
