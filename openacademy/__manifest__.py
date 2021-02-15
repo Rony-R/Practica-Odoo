f7f73500562363579a511b3ca9b0cc2c177c8998
Rony-R <ronyrodriguez2011@hotmail.com>
2021-02-15 04:56:56 +0000

    Security & Record Rules Completed

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -25,7 +25,7 @@
 
     # always loaded
     'data': [
-        #'security/security.xml',
+        'security/security.xml',
         'security/ir.model.access.csv',
         'templates.xml',
         'views/openacademy.xml',
