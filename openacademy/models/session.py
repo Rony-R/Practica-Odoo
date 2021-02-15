36c2b85d91fbc3b78d0c3e77fea609c6ab309fe7
Rony-R <ronyrodriguez2011@hotmail.com>
2021-02-15 02:15:41 +0000

    Constraints

--- openacademy/models/session.py
+++ openacademy/models/session.py
@@ -42,4 +42,8 @@ class Session(models.Model):
                 },
             }
 
-    
+    @api.constrains('instructor_id', 'attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")
